d2c2334ac80c98422f4affdfeb37d9dd6a6f129f
sin365 <353374337@qq.com>
2024-01-22 11:57:17 +0800

    服务器相关

--- NoSugarNet.ServerCore/Manager/LoginManager.cs
+++ NoSugarNet.ServerCore/Manager/LoginManager.cs
@@ -27,6 +27,15 @@ namespace ServerCore.Manager
             });
 
             ServerManager.g_ClientMgr.ClientSend(cinfo, (int)CommandID.CmdLogin, (int)ErrorCode.ErrorOk, respData);
+
+            Protobuf_Cfgs cfgsSP = new Protobuf_Cfgs();
+            cfgsSP.Cfgs.Add(new Protobuf_Cfgs_Single { TunnelID = 0, IP = "127.0.0.1", Port = 10001 });
+            cfgsSP.Cfgs.Add(new Protobuf_Cfgs_Single { TunnelID = 1, IP = "127.0.0.1", Port = 10002 });
+            cfgsSP.Cfgs.Add(new Protobuf_Cfgs_Single { TunnelID = 2, IP = "127.0.0.1", Port = 10003 });
+            cfgsSP.Cfgs.Add(new Protobuf_Cfgs_Single { TunnelID = 3, IP = "127.0.0.1", Port = 10004 });
+
+            byte[] respDataCfg = ProtoBufHelper.Serizlize(cfgsSP);
+            ServerManager.g_ClientMgr.ClientSend(cinfo, (int)CommandID.CmdCfgs, (int)ErrorCode.ErrorOk, respDataCfg);
         }
     }
 }
