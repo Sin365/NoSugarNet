06dc7559ba39f0adc6e9ae0d5ed5351a7c624bde
sin365 <353374337@qq.com>
2024-01-22 11:58:24 +0800

    Sample

---
+++ Sample/NoSugarNet.ClientCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using NoSugarNet.ClientCore;
+
+namespace NoSugarNet.ClientCli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppNoSugarNet.Init("127.0.0.1", 1000);
+        }
+    }
+}
---
+++ Sample/NoSugarNet.ServerCli/Program.cs
@@ -0,0 +1,16 @@
+﻿using ServerCore.Manager;
+
+namespace NoSugarNet.ServerCli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerManager.InitServer(1000);
+            while (true) 
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
